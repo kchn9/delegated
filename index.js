e145f2b1672000544436810f884af613c9b24ef9
kchn9 <mat.kuchnia@gmail.com>
2022-10-21 14:36:54 +0200

    Extract server variable

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ const http = require("http");
 const app = require("./app");
 const logger = require("./utils/logger");
 
-http.createServer(app).listen(config.PORT, () => {
+const server = http.createServer(app);
+
+server.listen(config.PORT, () => {
   logger.info(`Server is running on PORT: ${config.PORT}`);
 });
