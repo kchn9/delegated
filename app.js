e7896bd485bed66912630d95771c22e00f2041a8
kchn9 <mat.kuchnia@gmail.com>
2022-10-21 10:13:04 +0200

    Establish db connection

--- app.js
+++ app.js
@@ -1,6 +1,19 @@
+const config = require("./utils/config");
 const express = require("express");
-const cors = require("cors");
 const app = express();
+const cors = require("cors");
+const logger = require("./utils/logger");
+const mongoose = require("mongoose");
+
+logger.info(`Connecting to MongoDB@:${config.MONGODB_URI}`);
+mongoose
+  .connect(config.MONGODB_URI)
+  .then(() => {
+    logger.info("Connection established succesfully.");
+  })
+  .catch((err) => {
+    logger.error("Connection failed - following error has been caught: ", err);
+  });
 
 app.use(cors());
 app.use(express.json());
