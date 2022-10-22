c8ab9a1f9021b0c63faeb0585614e64781dd5ea0
kchn9 <mat.kuchnia@gmail.com>
2022-10-22 11:19:51 +0200

    Add describe blocks

--- tests/trip.test.js
+++ tests/trip.test.js
@@ -11,25 +11,27 @@ beforeEach(async () => {
   await Promise.all(promiseArray);
 });
 
-describe("Trip model", () => {
+describe("when there is initially some trips saved", () => {
   test("returns trips with properly parsed id", async () => {
     const trips = await helper.getTrips();
     trips.every((trip) => expect(trip.id).toBeDefined());
   });
 
-  test("creates trip with default title", async () => {
+  test("trips have default titles", async () => {
     const trips = await helper.getTrips();
     trips.every((trip) => expect(trip.title).toBeDefined());
   });
 
-  test("calculates daysLength properly", async () => {
+  test("trips have properly calculated daysLength", async () => {
     const selectedTrip = (await helper.getTrips())[0];
     const start = new Date(selectedTrip.startDate);
     const end = new Date(selectedTrip.endDate);
     const expected = (end - start) / (1000 * 60 * 60 * 24);
     expect(selectedTrip.daysLength).toBe(expected);
   });
+});
 
+describe("trip creation", () => {
   test("should create valid trip", async () => {
     const newTrip = new Trip({
       country: "Belgium",
@@ -43,24 +45,26 @@ describe("Trip model", () => {
     expect(countries).toContainEqual(newTrip.country);
   });
 
-  test("should not validate without country", async () => {
-    const trip = new Trip({
-      startDate: "2024-01-02T12:44:00",
-      endDate: "2025-10-21T15:18:00",
+  describe("validation", () => {
+    test("should not validate without country", async () => {
+      const trip = new Trip({
+        startDate: "2024-01-02T12:44:00",
+        endDate: "2025-10-21T15:18:00",
+      });
+      await expect(trip.validate()).rejects.toThrow(
+        mongoose.Error.ValidationError
+      );
     });
-    await expect(trip.validate()).rejects.toThrow(
-      mongoose.Error.ValidationError
-    );
-  });
 
-  test("should not validate with startDate exceeding endDate", async () => {
-    const trip = new Trip({
-      startDate: "2021-01-04T02:13:00",
-      endDate: "2020-10-11T13:46:00",
+    test("should not validate with startDate exceeding endDate", async () => {
+      const trip = new Trip({
+        startDate: "2021-01-04T02:13:00",
+        endDate: "2020-10-11T13:46:00",
+      });
+      await expect(trip.validate()).rejects.toThrow(
+        mongoose.Error.ValidationError
+      );
     });
-    await expect(trip.validate()).rejects.toThrow(
-      mongoose.Error.ValidationError
-    );
   });
 });
 
