b8a314f0efbd3f2045eaea58d8196c80ef87c2fc
kchn9 <mat.kuchnia@gmail.com>
2022-11-06 21:17:47 -0500

    Add map placeholder

--- frontend/src/pages/Dashboard/Map/Map.jsx
+++ frontend/src/pages/Dashboard/Map/Map.jsx
@@ -2,12 +2,14 @@ import styled from "styled-components";
 
 import SubTitle from "../../../components/SubTitle";
 
-const MapContainer = styled.div``;
+const MapContainer = styled.section`
+  text-align: center;
+`;
 
 export default function Map() {
   return (
     <MapContainer>
-      <SubTitle>Map</SubTitle>
+      <SubTitle>Coming soon</SubTitle>
     </MapContainer>
   );
 }
